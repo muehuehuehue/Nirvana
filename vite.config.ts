fae61930be779ce6fdd3df634af107e587b1e966
muehuehuehue <yungblud1997.dom@gmail.com>
2026-05-16 23:20:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/YOUR_REPO_NAME/',
+    base: 'Nirvana',
 
     plugins: [
       react(),
